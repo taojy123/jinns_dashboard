63c13632fdf349a560c147bbd5007514d6eab49f
taojy123 <taojy123@163.com>
2018-11-08 12:22:09 +0800

    update qiniu domain

--- plugins/config.js
+++ plugins/config.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 const qiniu = {
   UPLOAD_TOKEN: 'QzBPrPDb8Vu1V0c2KV4ZL73exW4MkcjViESE9QKI:ndpMRAnq1XTNSUbb_61NM9jkOHY=:eyJzY29wZSI6Imppbm5zIiwiZGVhZGxpbmUiOjE1MjczMzEwNTV9',
   BUCKET: 'jinns',
-  UPLOAD_DOMAIN: 'http://p7iw1e96j.bkt.clouddn.com',
+  UPLOAD_DOMAIN: 'http://qiniu.jinns.top',
   MAX_FILE_SIZE: 1024 * 1024 * 20
 }
 
